71b0d4595cbb0bdd15c9e34ecce274f4c44caa97
irga75 <ira.gurzhy@yandex.ru>
2023-02-28 19:10:55 +0300

    Stage

--- data/users.py
+++ data/users.py
@@ -1,11 +1,13 @@
 import datetime
 
 import sqlalchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from data.db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
@@ -18,6 +20,12 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now())
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
 
     def __repr__(self):
         return f'<Colonist> {self.id} {self.surname} {self.name}'
--- main.py
+++ main.py
@@ -1,23 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user
+
 from data import db_session
 from data.jobs import Jobs
-
-db_session.global_init("db/blogs.db")
-
-db_sess = db_session.create_session()
-
+from data.users import User
+from forms.login_form import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-
-@app.route('/')
-def jobs_table():
-    return render_template('index.html', jobs_list=db_sess.query(Jobs).all())
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 def main():
-    app.run(port=5000, host='127.0.0.1')
+    db_session.global_init("db/blogs.db")
+    app.run()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
